cee76676e71017fbc149649e3b78f4bfbe68e619
DungxLas <phamjhungfdungx@gmail.com>
2022-11-23 22:48:10 +0700

    update

--- QuanLyNganHang/Form1.Designer.cs
+++ QuanLyNganHang/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.btnThem = new System.Windows.Forms.Button();
             this.btnXoa = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.rBtnGuiCoKyHan = new System.Windows.Forms.RadioButton();
@@ -48,6 +48,11 @@
             this.label6 = new System.Windows.Forms.Label();
             this.cBxKyHanGui = new System.Windows.Forms.ComboBox();
             this.listViewDanhSachSo = new System.Windows.Forms.ListView();
+            this.clTenKhachHang = new System.Windows.Forms.ColumnHeader();
+            this.clCMND = new System.Windows.Forms.ColumnHeader();
+            this.clSoTienGui = new System.Windows.Forms.ColumnHeader();
+            this.clNgayLapSo = new System.Windows.Forms.ColumnHeader();
+            this.clKyhanLaisuat = new System.Windows.Forms.ColumnHeader();
             this.SuspendLayout();
             // 
             // label1
@@ -129,6 +134,7 @@
             this.btnXoa.TabIndex = 21;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // button3
             // 
@@ -139,14 +145,15 @@
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnSua
             // 
-            this.button4.Location = new System.Drawing.Point(278, 331);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 31);
-            this.button4.TabIndex = 22;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnSua.Location = new System.Drawing.Point(278, 331);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 31);
+            this.btnSua.TabIndex = 22;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // button5
             // 
@@ -203,10 +210,12 @@
             // 
             // dtpNgayLapSo
             // 
+            this.dtpNgayLapSo.CustomFormat = "";
             this.dtpNgayLapSo.Location = new System.Drawing.Point(233, 180);
             this.dtpNgayLapSo.Name = "dtpNgayLapSo";
             this.dtpNgayLapSo.Size = new System.Drawing.Size(323, 32);
             this.dtpNgayLapSo.TabIndex = 31;
+            this.dtpNgayLapSo.Value = new System.DateTime(2022, 11, 23, 0, 0, 0, 0);
             // 
             // label6
             // 
@@ -229,21 +238,59 @@
             // 
             // listViewDanhSachSo
             // 
+            this.listViewDanhSachSo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.listViewDanhSachSo.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listViewDanhSachSo.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clTenKhachHang,
+            this.clCMND,
+            this.clSoTienGui,
+            this.clNgayLapSo,
+            this.clKyhanLaisuat});
+            this.listViewDanhSachSo.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.listViewDanhSachSo.FullRowSelect = true;
             this.listViewDanhSachSo.GridLines = true;
-            this.listViewDanhSachSo.Location = new System.Drawing.Point(81, 399);
+            this.listViewDanhSachSo.Location = new System.Drawing.Point(63, 399);
             this.listViewDanhSachSo.Name = "listViewDanhSachSo";
-            this.listViewDanhSachSo.Size = new System.Drawing.Size(577, 347);
+            this.listViewDanhSachSo.Size = new System.Drawing.Size(606, 278);
             this.listViewDanhSachSo.TabIndex = 33;
             this.listViewDanhSachSo.TileSize = new System.Drawing.Size(1, 1);
             this.listViewDanhSachSo.UseCompatibleStateImageBehavior = false;
             this.listViewDanhSachSo.View = System.Windows.Forms.View.Details;
             // 
+            // clTenKhachHang
+            // 
+            this.clTenKhachHang.Text = "Tên Khách Hàng";
+            this.clTenKhachHang.Width = 120;
+            // 
+            // clCMND
+            // 
+            this.clCMND.Text = "CMND";
+            this.clCMND.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.clCMND.Width = 120;
+            // 
+            // clSoTienGui
+            // 
+            this.clSoTienGui.Text = "Số tiền gửi ban đầu";
+            this.clSoTienGui.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.clSoTienGui.Width = 120;
+            // 
+            // clNgayLapSo
+            // 
+            this.clNgayLapSo.Text = "Ngày lập sổ";
+            this.clNgayLapSo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.clNgayLapSo.Width = 120;
+            // 
+            // clKyhanLaisuat
+            // 
+            this.clKyhanLaisuat.Text = "Kỳ hạn / Lãi suất";
+            this.clKyhanLaisuat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.clKyhanLaisuat.Width = 120;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 666);
+            this.ClientSize = new System.Drawing.Size(698, 720);
             this.Controls.Add(this.listViewDanhSachSo);
             this.Controls.Add(this.lblKyHanGui);
             this.Controls.Add(this.cBxKyHanGui);
@@ -252,7 +299,7 @@
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnSua);
             this.Controls.Add(this.btnXoa);
             this.Controls.Add(this.btnThem);
             this.Controls.Add(this.rBtnGuiKoKyHan);
@@ -286,7 +333,7 @@
         private Button btnThem;
         private Button btnXoa;
         private Button button3;
-        private Button button4;
+        private Button btnSua;
         private Button button5;
         private Button button6;
         private RadioButton rBtnGuiCoKyHan;
@@ -296,5 +343,10 @@
         private Label label6;
         private ComboBox cBxKyHanGui;
         private ListView listViewDanhSachSo;
+        private ColumnHeader clTenKhachHang;
+        private ColumnHeader clCMND;
+        private ColumnHeader clSoTienGui;
+        private ColumnHeader clNgayLapSo;
+        private ColumnHeader clKyhanLaisuat;
     }
 }
--- QuanLyNganHang/Form1.cs
+++ QuanLyNganHang/Form1.cs
@@ -16,6 +16,7 @@ namespace QuanLyNganHang
             cBxKyHanGui.Items.Add("18 tháng / 8.40%");
 
             cBxKyHanGui.SelectedIndex = 0;
+
         }
 
         private void rBtnCoKyHan_Click(object sender, EventArgs e)
@@ -37,8 +38,13 @@ namespace QuanLyNganHang
             string cmnd = txtBxCMND.Text;
             double SoTienGui = double.Parse(txtBxSoTienGui.Text);
             DateTime NgayLapSo = dtpNgayLapSo.Value;
-            double LaiSuat = 5.65;
-            
+            double LaiSuat = 1;
+
+            string[] arr = new string[5]; //Tạo 1 mảng và nạp dữ liệu vào mảng đó
+            arr[0] = HoTenKhachHang;
+            arr[1] = cmnd;
+            arr[2] = SoTienGui.ToString();
+            arr[3] = NgayLapSo.ToString("dd/MM/yyyy");
 
             if (rBtnGuiCoKyHan.Checked == true)
             {
@@ -78,12 +84,47 @@ namespace QuanLyNganHang
                         }
                 }
 
-                CoKyHan stkCoKyHan = new CoKyHan(cmnd, HoTenKhachHang, SoTienGui, NgayLapSo, LaiSuat, KyHan);
+                //CoKyHan stkCoKyHan = new CoKyHan(cmnd, HoTenKhachHang, SoTienGui, NgayLapSo, LaiSuat, KyHan);
+
+                arr[4] = KyHan.ToString() + " tháng / " + LaiSuat.ToString() + "%";
             }
             else if (rBtnGuiKoKyHan.Checked == true)
             {
+                //KoKyHan stkKoKyHan = new KoKyHan(cmnd, HoTenKhachHang, SoTienGui, NgayLapSo, LaiSuat);
 
+                arr[4] = "Không kỳ hạn / " + LaiSuat.ToString() + "%";
             }
+
+            ListViewItem item = new ListViewItem(arr); //Tạo 1 list item ứng với từng dữ liệu
+
+            listViewDanhSachSo.Items.Add(item); //Nạp list item vào listView
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DialogResult dlr = MessageBox.Show("Bạn chắc chắn muốn xoá chứ ???", "Thông báo", MessageBoxButtons.YesNo);
+            
+            if (dlr == DialogResult.Yes)
+            {
+                for (int i = 0; i < listViewDanhSachSo.Items.Count; i++)
+                {
+                    if (listViewDanhSachSo.Items[i].Selected)
+                    {
+                        listViewDanhSachSo.Items[i].Remove();
+                        break;
+                    }
+                }
+            }
+            else if (dlr == DialogResult.No)
+            {
+                MessageBox.Show("Sau nhớ nghĩ kĩ rồi hay chọn", "Thông báo", MessageBoxButtons.OK);
+            }
+            
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
